582cb0594dc617c1d0e26549643bc1ff59c44513
saschazesiger <janis.zesiger@edu.tbz.ch>
2023-04-09 16:39:37 +0000

    Final UI Update

--- website/server.js
+++ website/server.js
@@ -1,17 +1,19 @@
+// Import required Node.js modules
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const env = require('dotenv').config();
 const simpleParser = require('mailparser').simpleParser;
-const engines = require('consolidate'); // Importiere die consolidate-Abhängigkeit
+const engines = require('consolidate');
 
-
-
+// Create an instance of the Express application
 const app = express();
 
+// Set up middleware to parse incoming requests with JSON and URL-encoded bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Create a connection to the MySQL database using environment variables
 const connection = mysql.createConnection({
     host: process.env.host,
     user: process.env.user,
@@ -21,21 +23,25 @@ const connection = mysql.createConnection({
     ssl: {}
 });
 
+// Serve static files from the public directory
 app.use(express.static(__dirname + '/public'));
 
-
+// Define a POST route for handling incoming emails
 app.post('/inbox', async (req, res) => {
-  console.log("New Email")
+    console.log("New Email");
 
+    // Check if the private key in the request payload matches the one in the environment variables
     if (req.body.key !== process.env.privatekey) {
         return res.status(403).send();
     }
 
+    // Parse the email data in the request payload using mailparser
     const parsedbody = await simpleParser(req.body.body)
 
+    // Construct an SQL INSERT statement to insert the email data into the MySQL database
     const sql = `INSERT INTO mail (sender, receiver, date, header, body) VALUES ("${parsedbody.from.text}", "${req.body.to}", NOW(), "${parsedbody.subject}", "${parsedbody.html.replaceAll('"', "'")}")`;
 
-
+    // Execute the SQL statement using the MySQL connection
     connection.query(sql, (err, result) => {
         if (err) {
             console.log(err);
@@ -45,49 +51,42 @@ app.post('/inbox', async (req, res) => {
     });
 });
 
-
-
+// Define a GET route for fetching email data
 app.get('/api/mail', (req, res) => {
     const email = req.query.email;
-
     const date = req.query.lastdate;
     
-    
+    // Subtract two hours from the last date and convert it to a MySQL datetime string
     newdate = new Date(date)
     newdate.setHours(newdate.getHours() - 2);
     const mysqlDate = newdate.toISOString().slice(0, 19).replace('T', ' ');
 
-  
-    // SQL-Abfrage, um E-Mail-Daten abzurufen
+    // Construct an SQL SELECT statement to fetch email data from the MySQL database
     const sql = `SELECT id,sender, receiver, header, body, date FROM mail WHERE receiver='${email}' AND date > '${mysqlDate}' ORDER BY date DESC LIMIT 50`;
-  
-    // Datenbankabfrage ausführen
+
+    // Execute the SQL statement using the MySQL connection
     connection.query(sql, (error, results, fields) => {
-      if (error) {
-        console.log(error)
-        res.status(500).json({ error: '0' });
-        return;
-      }
-  
-      // Überprüfen Sie, ob Daten gefunden wurden
-      if (results.length === 0) {
-        res.json([]);
-        return;
-      }
-  
-      // Extrahieren Sie die Daten aus der Abfrageergebnissen
-      const data = results;
-  
-      // Senden Sie das JSON-Objekt mit den abgerufenen Daten zurück
-      res.json(data);
+        if (error) {
+            console.log(error)
+            res.status(500).json({ error: '0' });
+            return;
+        }
+
+        // Check if any email data was found
+        if (results.length === 0) {
+            res.json([]);
+            return;
+        }
+
+        // Extract email data from the query results
+        const data = results;
+
+        // Send the email data as a JSON response
+        res.json(data);
     });
-  });
-  
+});
 
-
+// Start the server on port 5000 and log a message to the console
 app.listen(5000, () => {
     console.log('Server started on port 5000');
 });
-
-
-
